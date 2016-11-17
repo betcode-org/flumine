b5415544f27a5df69e4e5072f7517cfb1854084b
LiamPauling <paulingliam@gmail.com>
2016-11-17 20:41:47 +0000

    initial flumine class added with tests

--- flumine/flumine.py
+++ flumine/flumine.py
@@ -6,5 +6,29 @@ class Flumine:
         self.trading = trading
         self.strategies = strategies
 
+        self._socket = None
+
+    def start(self, market_filter, market_data_filter):
+        self._check_login()
+        self._create_socket()
+
+        self._socket.subscribe_to_markets(unique_id=2,
+                                          market_filter=market_filter,
+                                          market_data_filter=market_data_filter)
+        self._socket.start(async=True)
+
+    def stop(self):
+        self._socket.stop()
+
+    def _check_login(self):
+        if self.trading.session_expired:
+            self.trading.login()
+
+    def _create_socket(self):
+        self._socket = self.trading.streaming.create_stream(
+                unique_id=1,
+                description='Flumine Socket'
+        )
+
     def __str__(self):
         return '<Flumine>'
--- tests/test_flumine.py
+++ tests/test_flumine.py
@@ -15,5 +15,44 @@ class FlumineTest(unittest.TestCase):
         assert self.flumine.trading == self.trading
         assert self.flumine.strategies == self.strategies
 
+    @mock.patch('flumine.flumine.Flumine._create_socket')
+    @mock.patch('flumine.flumine.Flumine._check_login')
+    def test_start(self, mock_check_login, mock_create_socket):
+        market_filter = mock.Mock()
+        market_data_filter = mock.Mock()
+        mock_socket = mock.Mock()
+        self.flumine._socket = mock_socket
+
+        self.flumine.start(market_filter, market_data_filter)
+
+        mock_check_login.assert_called_with()
+        mock_create_socket.assert_called_with()
+        mock_socket.subscribe_to_markets.assert_called_with(
+                unique_id=2,
+                market_filter=market_filter,
+                market_data_filter=market_data_filter
+        )
+        mock_socket.start.assert_called_with(async=True)
+
+    def test_stop(self):
+        self.flumine._socket = mock.Mock()
+        self.flumine.stop()
+
+        self.flumine._socket.stop.assert_called_with()
+
+    def test_check_login(self):
+        self.trading.session_expired.return_value = True
+
+        self.flumine._check_login()
+        self.trading.login.assert_called_with()
+
+    def test_create_socket(self):
+        self.flumine._create_socket()
+
+        self.flumine.trading.streaming.create_stream.assert_called_with(
+                description='Flumine Socket',
+                unique_id=1
+        )
+
     def test_str(self):
         assert str(self.flumine) == '<Flumine>'
