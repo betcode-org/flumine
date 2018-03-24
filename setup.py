a3aa7ea7f905f66725e9aeb3a580f06bc2c239a3
LiamPauling <paulingliam@gmail.com>
2018-03-24 08:11:47 +0000

    docker file / requirements fix

--- setup.py
+++ setup.py
@@ -20,28 +20,28 @@ with open('flumine/__init__.py', 'r') as f:
     ).group(1)
 
 setup(
-        name='flumine',
-        version=version,
-        packages=[
-                'flumine',
-                'flumine.resources',
-                'flumine.storage'
-        ],
-        package_dir={
-                'flumine': 'flumine'
-        },
-        install_requires=INSTALL_REQUIRES,
-        url='https://github.com/liampauling/flumine',
-        license='MIT',
-        author='liampauling',
-        author_email='',
-        description='Betfair data record framework utilising betfair streaming',
-        classifiers=[
-                'License :: OSI Approved :: MIT License',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python :: 3.4',
-                'Programming Language :: Python :: 3.5',
-                'Programming Language :: Python :: 3.6',
-        ],
-        test_suite='tests'
+    name='flumine',
+    version=version,
+    packages=[
+        'flumine',
+        'flumine.resources',
+        'flumine.storage'
+    ],
+    package_dir={
+        'flumine': 'flumine'
+    },
+    install_requires=INSTALL_REQUIRES,
+    url='https://github.com/liampauling/flumine',
+    license='MIT',
+    author='liampauling',
+    author_email='',
+    description='Betfair data record framework utilising betfair streaming',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests'
 )
