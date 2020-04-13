e92d2c44371c1faa2524185fae5f2ab15b1fcf73
LiamPauling <paulingliam@gmail.com>
2020-04-13 13:34:30 +0100

    betfair order types added

--- flumine/order/trade.py
+++ flumine/order/trade.py
@@ -1,2 +1,15 @@
+import uuid
+
+
 class Trade:
-    pass
+    def __init__(
+        self, market_id: str, selection_id: int, strategy, fill_kill, green, stop
+    ):
+        self.id = uuid.uuid1()
+        self.market_id = market_id
+        self.selection_id = selection_id
+        self.strategy = strategy
+        self.fill_kill = fill_kill
+        self.green = green
+        self.stop = stop
+        self.orders = []  # all orders linked to trade
--- tests/test_trade.py
+++ tests/test_trade.py
@@ -6,7 +6,24 @@ from flumine.order.trade import Trade
 
 class TradeTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.trade = Trade()
+        self.mock_strategy = mock.Mock()
+        self.mock_fill_kill = mock.Mock()
+        self.mock_green = mock.Mock()
+        self.mock_stop = mock.Mock()
+        self.trade = Trade(
+            "1.234",
+            567,
+            self.mock_strategy,
+            self.mock_fill_kill,
+            self.mock_green,
+            self.mock_stop,
+        )
 
-    # def test_init(self):
-    #     self.assertEqual(self.trade._strategies, [])
+    def test_init(self):
+        self.assertEqual(self.trade.market_id, "1.234")
+        self.assertEqual(self.trade.selection_id, 567)
+        self.assertEqual(self.trade.strategy, self.mock_strategy)
+        self.assertEqual(self.trade.fill_kill, self.mock_fill_kill)
+        self.assertEqual(self.trade.green, self.mock_green)
+        self.assertEqual(self.trade.stop, self.mock_stop)
+        self.assertEqual(self.trade.orders, [])
