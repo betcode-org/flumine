ee703f1c29f6fd09aefa5c95a31412bdbe9432e0
LiamPauling <paulingliam@gmail.com>
2020-05-04 17:10:04 +0100

    _process_sp added to handle SP reconciliation

--- flumine/backtest/simulated.py
+++ flumine/backtest/simulated.py
@@ -8,16 +8,28 @@ from ..order.ordertype import OrderTypes
 
 
 class Simulated:
+    """
+    Class to hold `simulated` order
+    matching and status.
+    """
+
     def __init__(self, order):
         self.order = order
         self.matched = []
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self, market_book: MarketBook, traded: dict):
+        # simulates order matching
+        runner = self._get_runner(market_book)
+        if self.take_sp and market_book.bsp_reconciled:
+            self._process_sp(runner)  # todo simulate limitOrder with `TAKE SP`
+
+        if self.order.order_type.ORDER_TYPE == OrderTypes.LIMIT:
+            self._process_traded(runner, traded)
 
     def place(
         self, market_book: MarketBook, instruction: dict, bet_id: int
     ) -> SimulatedPlaceResponse:
+        # simulates placeOrder request->matching->response
         # todo instruction/fillkill/timeInForce/BPE etc
         if self.order.order_type.ORDER_TYPE == OrderTypes.LIMIT:
             runner = self._get_runner(market_book)
@@ -49,12 +61,15 @@ class Simulated:
             raise NotImplementedError()  # todo
 
     def cancel(self):
+        # simulates cancelOrder request->cancel->response
         pass
 
     def update(self):
+        # simulates updateOrder request->update->response
         pass
 
     def replace(self):
+        # simulates replaceOrder request->cancel/matching->response
         pass
 
     def _get_runner(self, market_book: MarketBook) -> RunnerBook:
@@ -67,6 +82,7 @@ class Simulated:
     def _process_price_matched(
         self, price: float, size: float, available: list
     ) -> None:
+        # calculate matched on execution
         size_remaining = size
         for avail in available:
             if size_remaining == 0:
@@ -85,6 +101,35 @@ class Simulated:
             else:
                 break
 
+    def _process_sp(self, runner: RunnerBook) -> None:
+        # calculate matched on BSP reconciliation
+        actual_sp = runner.sp.actual_sp
+        if actual_sp and self.size_remaining:
+            _order_type = self.order.order_type
+            if _order_type.ORDER_TYPE == OrderTypes.LIMIT:
+                size = self.size_remaining
+            elif _order_type.ORDER_TYPE == OrderTypes.LIMIT_ON_CLOSE:
+                if self.side == "BACK":
+                    if actual_sp < _order_type.price:
+                        return
+                    size = _order_type.liability
+                else:
+                    if actual_sp > _order_type.price:
+                        return
+                    size = round(_order_type.liability / (actual_sp - 1), 2)
+            elif _order_type.ORDER_TYPE == OrderTypes.MARKET_ON_CLOSE:
+                if self.side == "BACK":
+                    size = _order_type.liability
+                else:
+                    size = round(_order_type.liability / (actual_sp - 1), 2)
+            else:
+                raise NotImplementedError()
+            self.matched.append((actual_sp, size))
+
+    def _process_traded(self, runner: RunnerBook, traded: dict) -> None:
+        # calculate matched on MarketBook update
+        pass
+
     @staticmethod
     def _wap(matched: list) -> Tuple[float, float]:
         if not matched:
@@ -97,6 +142,15 @@ class Simulated:
             return 0, 0
         else:
             return round(b, 2), round(a / b, 2)
+
+    @property
+    def take_sp(self) -> bool:
+        if self.order.order_type.ORDER_TYPE == OrderTypes.LIMIT:
+            if self.order.order_type.persistence_type == "MARKET_ON_CLOSE":
+                return True
+            return False
+        else:
+            return True
 
     @property
     def side(self) -> str:
@@ -117,3 +171,7 @@ class Simulated:
             return size_matched
         else:
             return 0
+
+    @property
+    def size_remaining(self) -> float:
+        return self.order.order_type.size - self.size_matched
--- tests/test_simulated.py
+++ tests/test_simulated.py
@@ -20,6 +20,34 @@ class SimulatedTest(unittest.TestCase):
         self.assertEqual(self.simulated.matched, [])
 
     @mock.patch("flumine.backtest.simulated.Simulated._get_runner")
+    @mock.patch("flumine.backtest.simulated.Simulated.take_sp", return_value=True)
+    @mock.patch("flumine.backtest.simulated.Simulated._process_traded")
+    @mock.patch("flumine.backtest.simulated.Simulated._process_sp")
+    def test_call(self, mock__process_sp, mock__process_traded, _, mock__get_runner):
+        mock_market_book = mock.Mock()
+        mock_market_book.bsp_reconciled = True
+        self.simulated(mock_market_book, {})
+        mock__process_sp.assert_called_with(mock__get_runner())
+        mock__process_traded.assert_called_with(mock__get_runner(), {})
+
+    @mock.patch("flumine.backtest.simulated.Simulated._get_runner")
+    @mock.patch("flumine.backtest.simulated.Simulated.take_sp", return_value=True)
+    @mock.patch("flumine.backtest.simulated.Simulated._process_traded")
+    @mock.patch("flumine.backtest.simulated.Simulated._process_sp")
+    def test_call(self, mock__process_sp, mock__process_traded, _, mock__get_runner):
+        mock_market_book = mock.Mock()
+        mock_market_book.bsp_reconciled = False
+        self.simulated.order.order_type.ORDER_TYPE = OrderTypes.MARKET_ON_CLOSE
+        self.simulated(mock_market_book, {})
+        mock__process_sp.assert_not_called()
+        mock__process_traded.assert_not_called()
+
+        mock_market_book.bsp_reconciled = True
+        self.simulated(mock_market_book, {})
+        mock__process_sp.assert_called_with(mock__get_runner())
+        mock__process_traded.assert_not_called()
+
+    @mock.patch("flumine.backtest.simulated.Simulated._get_runner")
     def test_place_limit(self, mock__get_runner):
         mock_market_book = mock.Mock()
         mock_runner = mock.Mock()
@@ -109,6 +137,75 @@ class SimulatedTest(unittest.TestCase):
         )
         self.assertEqual(self.simulated.matched, [(3, 1)])
 
+    def test__process_sp(self):
+        mock_runner = mock.Mock()
+        mock_runner.sp.actual_sp = 12.20
+        self.simulated._process_sp(mock_runner)
+        self.assertEqual(self.simulated.matched, [(12.2, 2.00)])
+
+    def test__process_sp_none(self):
+        mock_runner = mock.Mock()
+        mock_runner.sp.actual_sp = None
+        self.simulated._process_sp(mock_runner)
+        self.assertEqual(self.simulated.matched, [])
+
+    def test__process_sp_processed(self):
+        mock_runner = mock.Mock()
+        mock_runner.sp.actual_sp = 12.20
+        self.simulated.matched = [(12.2, 2)]
+        self.simulated._process_sp(mock_runner)
+        self.assertEqual(self.simulated.matched, [(12.2, 2)])
+
+    def test__process_sp_processed_semi(self):
+        mock_runner = mock.Mock()
+        mock_runner.sp.actual_sp = 12.20
+        self.simulated.matched = [(10.0, 1)]
+        self.simulated._process_sp(mock_runner)
+        self.assertEqual(self.simulated.matched, [(10.0, 1), (12.2, 1)])
+
+    def test__process_sp_limit_on_close(self):
+        mock_limit_on_close_order = mock.Mock(price=10.0, size=2.00, liability=2.00)
+        mock_limit_on_close_order.ORDER_TYPE = OrderTypes.LIMIT_ON_CLOSE
+        self.simulated.order.order_type = mock_limit_on_close_order
+        mock_runner_book = mock.Mock()
+        mock_runner_book.sp.actual_sp = 69
+        self.simulated._process_sp(mock_runner_book)
+        self.assertEqual(self.simulated.matched, [(69, 2.00)])
+
+    def test__process_sp_limit_on_close_lay(self):
+        mock_limit_on_close_order = mock.Mock(price=100.0, size=2.00, liability=2.00)
+        mock_limit_on_close_order.ORDER_TYPE = OrderTypes.LIMIT_ON_CLOSE
+        self.simulated.order.order_type = mock_limit_on_close_order
+        self.simulated.order.side = "LAY"
+        mock_runner_book = mock.Mock()
+        mock_runner_book.sp.actual_sp = 69
+        self.simulated._process_sp(mock_runner_book)
+        self.assertEqual(self.simulated.matched, [(69, 0.03)])
+
+    def test_syn_process_sp_market_on_close(self):
+        mock_limit_on_close_order = mock.Mock(size=2.00, liability=2.00)
+        mock_limit_on_close_order.ORDER_TYPE = OrderTypes.MARKET_ON_CLOSE
+        self.simulated.order.order_type = mock_limit_on_close_order
+
+        mock_runner_book = mock.Mock()
+        mock_runner_book.sp.actual_sp = 69
+        self.simulated._process_sp(mock_runner_book)
+        self.assertEqual(self.simulated.matched, [(69, 2.00)])
+
+    def test_syn_process_sp_market_on_close_lay(self):
+        mock_limit_on_close_order = mock.Mock(size=2.00, liability=2.00)
+        mock_limit_on_close_order.ORDER_TYPE = OrderTypes.MARKET_ON_CLOSE
+        self.simulated.order.order_type = mock_limit_on_close_order
+        self.simulated.order.side = "LAY"
+
+        mock_runner_book = mock.Mock()
+        mock_runner_book.sp.actual_sp = 69
+        self.simulated._process_sp(mock_runner_book)
+        self.assertEqual(self.simulated.matched, [(69, 0.03)])
+
+    # def test__process_traded(self):
+    #     pass
+
     def test_wap(self):
         matched = [(1.5, 100), (1.6, 100)]
         assert self.simulated._wap(matched) == (200, 1.55)
@@ -121,6 +218,19 @@ class SimulatedTest(unittest.TestCase):
         matched = [(1.5, 0)]
         assert self.simulated._wap(matched) == (0, 0)
 
+    def test_take_sp(self):
+        self.assertFalse(self.simulated.take_sp)
+        self.simulated.order.order_type.ORDER_TYPE = OrderTypes.LIMIT_ON_CLOSE
+        self.assertTrue(self.simulated.take_sp)
+        self.simulated.order.order_type.ORDER_TYPE = OrderTypes.MARKET_ON_CLOSE
+        self.assertTrue(self.simulated.take_sp)
+        self.simulated.order.order_type.ORDER_TYPE = OrderTypes.LIMIT
+        self.simulated.order.order_type.persistence_type = "MARKET_ON_CLOSE"
+        self.assertTrue(self.simulated.take_sp)
+
+    def test_side(self):
+        self.assertEqual(self.simulated.side, self.mock_order.side)
+
     def test_average_price_matched(self):
         self.assertEqual(self.simulated.average_price_matched, 0)
         self.simulated.matched = [(1, 2)]
@@ -131,5 +241,12 @@ class SimulatedTest(unittest.TestCase):
         self.simulated.matched = [(1, 2)]
         self.assertEqual(self.simulated.size_matched, 2)
 
-    def test_side(self):
-        self.assertEqual(self.simulated.side, self.mock_order.side)
+    @mock.patch(
+        "flumine.backtest.simulated.Simulated.size_matched",
+        new_callable=mock.PropertyMock,
+    )
+    def test_size_remaining(self, mock_size_matched):
+        mock_size_matched.return_value = 0
+        self.assertEqual(self.simulated.size_remaining, 2)
+        mock_size_matched.return_value = 1
+        self.assertEqual(self.simulated.size_remaining, 1)
