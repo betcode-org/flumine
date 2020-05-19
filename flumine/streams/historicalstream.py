9345bd1dfe89592ac76929fa58961d6f03a0afae
jon <jon@none.com>
2020-05-19 21:18:40 +0100

    Refactoring HistoricalStream to use the new update_generator param, which has replaed the directory param.

--- flumine/streams/historicalstream.py
+++ flumine/streams/historicalstream.py
@@ -19,6 +19,6 @@ class HistoricalStream(BaseStream):
     def create_generator(self):
         self._listener.register_stream(0, "marketSubscription")
         stream = HistoricalGeneratorStream(
-            directory=self.market_filter, listener=self._listener
+            update_generator=self.market_filter, listener=self._listener
         )
         return stream.get_generator()
