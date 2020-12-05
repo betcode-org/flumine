e4cdf79f700e1b722efb9ec2b9069c94c15bdf9b
Lee Unsworth <lunswor@gmail.com>
2020-12-05 11:07:16 +0000

    Adds comment of what the MarketCatalogueMiddleware class does and how to use

--- examples/middlewares/marketcatalogue.py
+++ examples/middlewares/marketcatalogue.py
@@ -4,9 +4,11 @@ import os
 from betfairlightweight.resources import MarketCatalogue
 from flumine.markets.middleware import Middleware
 
-MARKET_CATALOGUE_PATH = ""
+MARKET_CATALOGUE_PATH = ""  # update to correct path
 
 
+# Will read and parse the market_catalogue file and add to the market object when backtesting
+# Usage framework.add_middleware(MarketCatalogueMiddleware())
 class MarketCatalogueMiddleware(Middleware):
     def __call__(self, market) -> None:
         catalogue_file_path = os.path.join(MARKET_CATALOGUE_PATH, market.market_id)
