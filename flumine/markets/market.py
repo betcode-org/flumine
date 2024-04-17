48b0b57d61bb22fc123d7c4bab9717707118b676
Maurice Berk <maurice@mauriceberk.com>
2024-04-17 06:25:56 +0100

    Handle market catalogue missing description

--- flumine/markets/market.py
+++ flumine/markets/market.py
@@ -137,7 +137,7 @@ class Market:
 
     @property
     def market_type(self) -> str:
-        if self.market_catalogue:
+        if self.market_catalogue and self.market_catalogue.description:
             return self.market_catalogue.description.market_type
         elif self.market_book:
             return self.market_book.market_definition.market_type
@@ -189,7 +189,7 @@ class Market:
 
     @property
     def race_type(self) -> Optional[str]:
-        if self.market_catalogue:
+        if self.market_catalogue and self.market_catalogue.description:
             return self.market_catalogue.description.race_type
         elif self.market_book:
             return self.market_book.market_definition.race_type
