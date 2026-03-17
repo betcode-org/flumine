b7a49bce76e0cfadf89dc935d23fe7a99048a043
liampauling <paulingliam@gmail.com>
2026-03-17 10:32:16 +0000

    remove print

--- flumine/execution/betfairexecution.py
+++ flumine/execution/betfairexecution.py
@@ -26,7 +26,6 @@ class BetfairExecution(BaseExecution):
                     self._order_logger(
                         order, instruction_report, OrderPackageType.PLACE
                     )
-                    print(instruction_report.__dict__)
                     if instruction_report.status == "SUCCESS":
                         if instruction_report.order_status == "PENDING":
                             pass  # async request pending processing
